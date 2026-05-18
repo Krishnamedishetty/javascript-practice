4424712b131e00edfa2564817cfcf63c17867324
krishna <129498379+Krishnamedishetty@users.noreply.github.com>
2026-05-18 18:33:43 +0000

    IIFE Done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function coffee() {
+    // Named IIFE
+    console.log(`Hello World`);
+})();
+
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)   
+}) (`krishna`)
+
